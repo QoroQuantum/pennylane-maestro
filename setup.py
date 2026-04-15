728283572f145f01eea29eabcf4c693fad70861d
stephendiadamo <stephen.diadamo@gmail.com>
2026-04-15 16:15:51 +0200

    fix: version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pennylane-maestro",
-    version="0.1.0",
+    version="0.2.0",
     description="PennyLane plugin for the Maestro quantum simulator by Qoro Quantum",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
