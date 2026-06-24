0b297341c5791f5357f60ea90f7a0a0af733b1c9
Stephen DiAdamo <stephen.diadamo@gmail.com>
2026-06-24 16:18:03 -0400

    chore: bump package version to 0.3.1

--- pennylane_maestro/__init__.py
+++ pennylane_maestro/__init__.py
@@ -2,5 +2,5 @@
 
 from pennylane_maestro.maestro_device import MaestroQubitDevice
 
-__version__ = "0.2.4"
+__version__ = "0.3.1"
 __all__ = ["MaestroQubitDevice"]
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pennylane-maestro",
-    version="0.3.0",
+    version="0.3.1",
     description="PennyLane plugin for the Maestro quantum simulator by Qoro Quantum",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
